ceff78d987bc258d932b96986977264123677a7a
rodionM87 <65753876+rodionM87@users.noreply.github.com>
2023-03-03 12:43:43 +0600

    Update FormulaCreator.java

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a;
+        int b;
+        double f;
+        f = Math.pow(a, 2);
+        System.out.println((9 * f - 5 * b + 2 + a - 7)*((a + b - 4 * a * b) / 2));
     }
 }
